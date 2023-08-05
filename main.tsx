d532c08d26d250b838d4d4ccc712f43fe7e016fb
kungfunk <victor.calzon@auctane.com>
2023-08-05 20:16:42 +0200

    add styles and task dates

--- main.tsx
+++ main.tsx
@@ -1,18 +1,20 @@
 /** @jsx jsx */
 import { Hono } from 'https://deno.land/x/hono@v3.3.4/mod.ts';
-import { jsx, html } from 'https://deno.land/x/hono@v3.3.4/middleware.ts';
+import { jsx, html, serveStatic } from 'https://deno.land/x/hono@v3.3.4/middleware.ts';
+import { format } from "https://deno.land/std@0.197.0/datetime/format.ts";
 
 const app = new Hono();
 
 interface Task {
   id: number;
   name: string;
+  date: string;
 }
 
 const tasks: Task[] = [
-  { id: 1, name: 'Task 1' },
-  { id: 2, name: 'Task 2' },
-  { id: 3, name: 'Task 3' },
+  { id: 1, name: 'Lore ipsum dolor sit amet', date: '2023-08-05 08:09:10.562' },
+  { id: 2, name: 'Cras sit amet arcu ut nunc aliquet feugiat at suscipit augue', date: '2023-08-04 03:01:10.233' },
+  { id: 3, name: 'Vivamus finibus nulla nec posuere lacinia. Quisque eleifend nec quam et cursus. Duis orci augue, dignissim sit amet blandit eget, porta vitae purus. ', date: '2023-08-05 08:09:10.562' },
 ];
 
 const Layout = ({ children }: { children: any }) => html`
@@ -21,40 +23,63 @@ const Layout = ({ children }: { children: any }) => html`
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    <link rel="stylesheet" type="text/css" href="/static/styles.css">
     <script src="https://unpkg.com/htmx.org@1.9.4"></script>
     <title>Task manager</title>
   </head>
   <body>
-    <main class="container">
       ${children}
-    </main>
   </body>
 </html>
 `;
 
 const TaskList = ({ tasks }: { tasks: Task[] }) => {
-  return <ul id="tasks">{tasks.map(task => <li>{task.name}</li>)}</ul>;
+  return (
+    <ul class="tasklist" id="tasklist">{tasks.map(({id, name, date}) =>
+      <li class="task">
+        <input type="checkbox" class="task__checkbox" hx-delete={"/task/"+id}  hx-target="#tasklist" hx-swap="outerHTML swap:1s" />
+        <div>
+          <p class="task__name">{name}</p>
+          <p class="task__date">{date}</p>
+        </div>
+      </li>)}
+    </ul>
+  );
 }
 
 app.get('/', (c) => {
   return c.html(
     <Layout>
-      <h1>Tasks</h1>
-      <TaskList tasks={tasks} />
-      <h2>Add new Task</h2>
-      <form hx-post="/task" hx-target="#tasks" hx-swap="outerHTML">
-        <input type="text" name="name" placeholder="Task name" />
-        <button type="submit">Add Task</button>
-      </form>
+      <main class="container">
+        <h1 class="title">Todo List ✏️</h1>
+        <p class="tip">💡 You can share this URL and colaborate</p>
+        <form class="taskform" hx-post="/task" hx-target="#tasklist" hx-swap="outerHTML">
+          <input class="taskform__text" type="text" name="name" placeholder="Task name" />
+          <button class="taskform__button" type="submit">Add</button>
+        </form>
+        <TaskList tasks={tasks} />
+        <footer class="footer">
+          <p class="footer__text">from kungfunk with ❤️</p>
+        </footer>
+      </main>
     </Layout>
   );
 });
 
 app.post('/task', async (c) => {
   const body = await c.req.parseBody();
-  tasks.push({ id: tasks.length + 1, name: body.name });
+  const id = tasks.length + 1;
+  const name = body.name as string;
+  const date = format(new Date(), "yyyy-MM-dd hh:mm:ss.SSS");
+  tasks.push({ id, name: name, date });
   return c.html(<TaskList tasks={tasks} />);
 });
 
+app.delete('/task/:id', (c) => {
+  const id = parseInt(c.req.param('id'));
+  tasks.splice(tasks.findIndex(task => task.id === id), 1);
+  return c.html(<TaskList tasks={tasks} />);
+});
+
+app.use('/static/*', serveStatic({ root: './' }))
 Deno.serve(app.fetch);
